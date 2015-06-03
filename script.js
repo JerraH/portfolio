2da292d1d9319cdb449f71ac1969307fa9813b7d
JerraH <jerra.haynes@gmail.com>
2015-06-03 15:10:19 -0500

    adding javascript for better control

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var cw = $('.c-backdrop').width();
+var cwp = $(cw*.1);
+	$('.c-backdrop').css({
+    'height': cw + 'px'
+	});
+
+var ch = $('.c-backdrop').height();
+var chp = $(ch*.1);
+	$('.curtains').css({
+		'height': ch + 'px'
+	});
